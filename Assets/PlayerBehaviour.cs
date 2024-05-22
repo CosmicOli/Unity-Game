86edde7ad4e91f704fa1838fd9483da09c180991
Sam Baker <cosmicoli13@gmail.com>
2024-05-22 23:22:31 +0100

    Added a player character and a floor

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    public float runningPower;
+    public float jumpingPower;
+
+    private bool isFacingRight;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFacingRight && horizontal > 0f)
+        {
+            FlipCharacter();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(horizontal * runningPower, rigidBody.velocity.y);
+
+    }
+
+    private void FlipCharacter()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        // Note that this means only horizontal values are added
+        // For keyboard controls, w and s could hence be removed
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
+            isGrounded = false;
+        }
+
+        if (context.canceled && rigidBody.velocity.y > 0f)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
+        }
+    }
+}
