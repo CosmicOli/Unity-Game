64990ca9c7ec404587b902aad77ec6a5ef24ad34
Sam Baker <cosmicoli13@gmail.com>
2024-05-23 17:50:53 +0100

    Changed sword behavior to default the direction to the last horizontal direction if no movement input is given

--- Assets/AttackBehaviour.cs
+++ Assets/AttackBehaviour.cs
@@ -6,14 +6,24 @@ using UnityEngine.InputSystem;
 public class AttackBehaviour : MonoBehaviour
 {
     public Vector2 currentDirection;
+    private Vector2 lastHorizontalDirection;
 
     public Vector2 GetDirection(InputAction.CallbackContext context)
     {
         Vector2 inputDirection = context.ReadValue<Vector2>();
 
+        if (Mathf.Abs(inputDirection.x) > 0 && inputDirection.y == 0)
+        {
+            lastHorizontalDirection = inputDirection;
+        }
+
         if (inputDirection != Vector2.zero)
         {
             currentDirection = inputDirection;
+        }
+        else
+        {
+            currentDirection = lastHorizontalDirection;
         }
 
         return currentDirection;
