160f18e1b570f5a6aae20c1d169955613ee5a6e3
Sam Baker <cosmicoli13@gmail.com>
2024-05-26 08:40:56 +0100

    Added the framework for entity AIs

--- Assets/BoxEnemyBehavior.cs
+++ Assets/BoxEnemyBehavior.cs
@@ -1,8 +1,51 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxEnemyBehavior : GenericGravityEntityBehaviour
+public class BoxEnemyBehavior : GenericGravityEntityBehaviour, EntityAIInterface
 {
+    public bool currentlyAttacking { get; set; }
 
+    public bool ableToAttack { get; set; }
+
+    public int aggroLevel { get; set; }
+
+    private GameObject player;
+
+    public int DetermineAggroLevel(GameObject entityAggroedOn)
+    {
+        return 0;
+    }
+
+    public void MoveUnaggroed()
+    {
+        
+    }
+
+    public void MoveAggroed(int aggroLevel, GameObject entityAggroedOn)
+    {
+
+    }
+
+    public void Attack(int aggroLevel, GameObject entityAggroedOn)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        EntityAI.ProgressAI(currentlyAttacking, ableToAttack, aggroLevel, player, DetermineAggroLevel, MoveUnaggroed, MoveAggroed, Attack);
+    }
 }
---
+++ Assets/EntityAI.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+public interface EntityAIInterface
+{
+    // This variable is used to check whether the entity is currently in an attack
+    public bool currentlyAttacking { get; set;}
+
+    // This variable is used to check whether the entity is currently able to attack
+    public bool ableToAttack { get; set; }
+
+    // This variable is used to check whether the entity has been aggroed
+    // Level 0 is unaggroed, and higher values refer to different states the entity is in
+    public int aggroLevel { get; set; }
+
+    // Determines what the current aggro level is based on information on other entities
+    public int DetermineAggroLevel(GameObject entityAggroedOn);
+
+    // Determines how the entity will move when not aggroed
+    public void MoveUnaggroed();
+
+    // Determines how the entity will move when aggroed
+    public void MoveAggroed(int aggroLevel, GameObject entityAggroedOn);
+
+    // Determines how the entity will attack
+    public void Attack(int aggroLevel, GameObject entityAggroedOn);
+}
+
+public static class EntityAI
+{
+    public static void ProgressAI(bool currentlyAttacking, bool ableToAttack, int aggroLevel, GameObject entityAggroedOn, Func<GameObject, int> DetermineAggroLevel, Action MoveUnaggroed, Action<int, GameObject> MoveAggroed, Action<int, GameObject> Attack)
+    {
+        // If currently not doing an attack
+        if (!currentlyAttacking)
+        {
+            // Find the aggro level
+            aggroLevel = DetermineAggroLevel(entityAggroedOn);
+
+            // If not aggroed
+            if (aggroLevel == 0)
+            {
+                MoveUnaggroed();
+            }
+            // If aggroed
+            else
+            {
+                // If able to attack
+                if (ableToAttack)
+                {
+                    Attack(aggroLevel, entityAggroedOn);
+                }
+                else
+                {
+                    MoveAggroed(aggroLevel, entityAggroedOn);
+                }
+            }
+        }
+    }
+}
